9ca29593282a8eab5e4b44b614fc2f96fab50799
jujWang96 <jujwang@ucdavis.edu>
2024-01-01 16:50:56 -0800

    update plot function

--- sim_util/plot_circles.m
+++ sim_util/plot_circles.m
@@ -1,12 +1,11 @@
 %author: Minjie Fan@UCDavis/Google
 
-function [] = plot_circles(loc, radius,lw,colr)
+function [] = plot_circles(loc, radius)
 
-if nargin==3
-    colr = [0.6 0.6 0.6];
-end
+GRAY = [0.6 0.6 0.6];
+
 for i = 1:length(radius)
-    viscircles(loc(i, :), radius(i), 'EdgeColor', colr, 'LineWidth', lw);
+    viscircles(loc(i, :), radius(i), 'EdgeColor', GRAY, 'LineWidth', 1);
 end
 
 end
